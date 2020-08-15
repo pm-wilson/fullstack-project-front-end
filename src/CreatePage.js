750b749a1aefc4b0e59c1d7ff28d584e93c5cc74
Patrick Wilson <pmwilson333@gmail.com>
2020-08-14 23:55:45 -0700

    front end working

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createDestination } from './FetchFunctions.js';
+import { createDestination, fetchAgents } from './FetchFunctions.js';
 
 class CreatePage extends React.Component {
     state = {
@@ -7,6 +7,17 @@ class CreatePage extends React.Component {
       city: '',
       flight_hours: 0,
       need_passport: '',
+      map_img: '',
+      agent_id: 1,
+      agents: [],
+    }
+
+    componentDidMount = async () => {
+      const agentsData = await fetchAgents();
+
+      this.setState({
+        agents: agentsData.body,
+      })
     }
 
     handleCountryChange = e => {
@@ -25,16 +36,27 @@ class CreatePage extends React.Component {
       this.setState({ need_passport: e.target.value === 'yes' ? true : false });
     }
 
+    handleMapChange = e => {
+      this.setState({ map_img: e.target.value });
+    }
+
+    handleAgentChange = e => {
+      this.setState({ agent_id: Number(e.target.value) });
+    }
+
     handleSubmit = async (e) => {
       e.preventDefault();
 
-      if(this.state.country !== '' && this.state.city !== '' && !isNaN(this.state.flight_hours) && this.state.flight_hours > 0 && this.state.need_passport !=='') {
+      if(this.state.map_img !== '' && this.state.country !== '' && this.state.city !== '' && !isNaN(this.state.flight_hours) && this.state.flight_hours > 0 && this.state.need_passport !=='') {
 
         await createDestination({
           country: this.state.country,
           city: this.state.city,
           flight_hours: this.state.flight_hours,
-          need_passport: this.state.need_passport
+          need_passport: this.state.need_passport,
+          map_img: this.state.map_img,
+          agent_id: this.state.agent_id,
+          user_id: 1,
         });
   
         alert(`Updated ${this.state.city}, ${this.state.country}!`)
@@ -43,8 +65,13 @@ class CreatePage extends React.Component {
           country: '',
           city: '',
           flight_hours: 0,
-          need_passport: ''
+          need_passport: '',
+          map_img: '',
+          agent_id: 1,
         });
+
+        this.props.history.push('/');
+        
       } else {
         alert('Please complete all form fields to create a new city')
       }
@@ -59,13 +86,13 @@ class CreatePage extends React.Component {
           <h3 className='center-this'>New Destination</h3>
             <form onSubmit={this.handleSubmit}>
               <label>City
-                <input onChange={this.handleCityChange}/>
+                <input onChange={this.handleCityChange} value={this.state.city}/>
               </label>
               <label>Country
-                <input  onChange={this.handleCountryChange}/>
+                <input  onChange={this.handleCountryChange} value={this.state.country}/>
               </label>
               <label>Flight Hours
-                <input onChange={this.handleFlightChange} type='number'/>
+                <input onChange={this.handleFlightChange} type='number' value={this.state.flight_hours}/>
               </label>
               <label> 
                 <input onChange={this.handlePassportChange} type='radio' name='passport' value='yes'/>
@@ -74,6 +101,18 @@ class CreatePage extends React.Component {
               <label>
                 <input onChange={this.handlePassportChange} type='radio' name='passport' value='no'/>
                 Passport not Required
+              </label>
+              <label>
+                Map Image URL
+                <input onChange={this.handleMapChange} value={this.state.map_img}/>
+              </label>
+              <label>
+                Agent:
+                <select onChange={this.handleAgentChange} value={this.state.agent_id}>
+                  {
+                    this.state.agents.map((agent, i) => <option key={'agent-' + i} value={agent.id}>{agent.agent_name}</option>)
+                  }
+                </select>
               </label>
               <button>Create Destination</button>
             </form>
--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,30 +1,151 @@
 import React from 'react';
-import { fetchDestination } from './FetchFunctions.js';
+import { fetchDestination, updateDestination, fetchAgents, deleteDestination } from './FetchFunctions.js';
 import { Link } from 'react-router-dom';
 
 class DetailPage extends React.Component {
-    state = {
-      destination: {},
-    }
+  state = {
+    country: '',
+    city: '',
+    flight_hours: 0,
+    need_passport: '',
+    map_img: '',
+    agent_id: 1,
+    agents: [],
+  }
 
-    componentDidMount = async () => {
-      const data = await fetchDestination(this.props.match.params.id);
+  componentDidMount = async () => {
+    const agentsData = await fetchAgents();
+    const data = await fetchDestination(this.props.match.params.id);
+    const agentId = agentsData.body.filter((agent) => {
+      if (agent.agent_name === data.body.agent_name) {
+        return true;
+      }
+      return false;
+    });
+    
+    this.setState({
+      agents: agentsData.body,
+      destination: data.body,    
+      country: data.body.country,
+      city: data.body.city,
+      flight_hours: data.body.flight_hours,
+      need_passport: data.body.need_passport,
+      map_img: data.body.map_img,
+      agent_id: agentId[0].id,
+    })
+  }
+
+  handleCountryChange = e => {
+    this.setState({ country: e.target.value });
+  }
+
+  handleCityChange = e => {
+    this.setState({ city: e.target.value });
+  }
+
+  handleFlightChange = e => {
+    this.setState({ flight_hours: Number(e.target.value) });
+  }
+
+  handlePassportChange = e => {
+    this.setState({ need_passport: e.target.value === 'yes' ? true : false });
+  }
+
+  handleMapChange = e => {
+    this.setState({ map_img: e.target.value });
+  }
+
+  handleAgentChange = e => {
+    this.setState({ agent_id: e.target.value });
+  }
+
+  handleDeleteDestination = async () => {
+    await deleteDestination(Number(this.props.match.params.id));
+    alert("This destination has been deleted");
+    
+    this.props.history.push('/');
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if(this.state.map_img !== '' && this.state.country !== '' && this.state.city !== '' && !isNaN(this.state.flight_hours) && this.state.flight_hours > 0 && this.state.need_passport !=='') {
+
+      await updateDestination(
+        Number(this.props.match.params.id),
+        {
+        country: this.state.country,
+        city: this.state.city,
+        flight_hours: this.state.flight_hours,
+        need_passport: this.state.need_passport,
+        map_img: this.state.map_img,
+        agent_id: this.state.agent_id,
+        user_id: 1,
+      });
+
+      alert(`Updated ${this.state.city}, ${this.state.country}!`)
 
       this.setState({
-        destination: data.body[0],
-      })
-    }
-    
-    render() {
-      return (
-        <div className='detail-view'>
-          <div className="detail-container">
-              Wishing to go to {this.state.destination.city}, {this.state.destination.country}? It is only {this.state.destination.flight_hours} hours away {this.state.destination.need_passport && 'and do not forget the passport'}!
-          </div>
-          <Link to='/'>Return to List Page</Link>
-        </div>
-      );
+        country: '',
+        city: '',
+        flight_hours: 0,
+        need_passport: '',
+        map_img: '',
+        agent_id: 1,
+        destination: {},
+      });
+
+      this.props.history.push('/');
+      
+    } else {
+      alert('Please complete all form fields to create a new city')
     }
   }
-  
-  export default DetailPage;
+
+  render() {
+    return (
+      <div className='create-area detail-container'>
+        <h3 className='center-this'>Destination Details</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>City
+            <input onChange={this.handleCityChange} value={this.state.city}/>
+          </label>
+          <label>Country
+            <input  onChange={this.handleCountryChange} value={this.state.country}/>
+          </label>
+          <label>Flight Hours
+            <input onChange={this.handleFlightChange} type='number' value={this.state.flight_hours}/>
+          </label>
+          <label> 
+            <input onChange={this.handlePassportChange} type='radio' name='passport' value='yes' checked={this.state.need_passport}/>
+            Passport is Required
+          </label>
+          <label>
+            <input onChange={this.handlePassportChange} type='radio' name='passport' value='no' checked={!this.state.need_passport}/>
+            Passport not Required
+          </label>
+          <label>
+            Map Image URL
+            <input onChange={this.handleMapChange} value={this.state.map_img}/>
+          </label>
+          <label>
+            Agent:
+            <select onChange={this.handleAgentChange} value={this.state.agent_id}>
+              {
+                this.state.agents.map((agent, i) => <option key={'agent-' + i} value={agent.id}>{agent.agent_name}</option>)
+              }
+            </select>
+          </label>
+          <button>Update Destination</button>
+        </form>
+        <div className='map-container'>
+          <img src={this.state.map_img} alt={this.state.country + ' Map'}/>
+        </div>
+        <Link to='/'>Return to List Page</Link>
+        <button onClick={this.handleDeleteDestination}>Remove Destination</button>
+      </div>
+    );
+  }
+}
+
+export default DetailPage;
